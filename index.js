fbbdb80fca42b458c074bc362b32fa0f47a86ae2
Timothy Barasa <timbaros87@gmail.com>
2024-04-10 10:47:21 +0300

    Adds DOMContentLoaded Event

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function (){
+    console.log("The DOM has loaded")
+
+     // Select the paragraph element with id="text"
+    const text = document.querySelector('#text');
+
+     //Update the text content
+    text.textContent = "This is really cool!";
+    console.log(text);
+}
+)
+console.log("I loaded beofore the DOM Loaded.");
